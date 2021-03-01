959d0ec897023dd6245453ea1117b94b8b18396c
brhon <54709839+brohwn@users.noreply.github.com>
2021-03-01 02:17:53 +0100

    fix: :bug: changing NODE to -> NODE_ENV

--- client/src/services/feedback.js
+++ client/src/services/feedback.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
 class FeedbackService {
-  //
-  url;
-  constructor() {
-    process.env.NODE === "production"
-      ? (this.url = "https://feedback-nest-prisma.herokuapp.com")
-      : (this.url = "http://localhost:8000");
-  }
+    //
+    url;
+    constructor() {
+        process.env.NODE_ENV === "production"
+            ? (this.url = "https://feedback-nest-prisma.herokuapp.com")
+            : (this.url = "http://localhost:8000");
+    }
 
-  getAll() {
-    return axios.get(this.url + "/feedback/all");
-  }
+    getAll() {
+        return axios.get(this.url + "/feedback/all");
+    }
 
-  addNewFeedback(data) {
-    console.log("oui");
-    return axios.post(this.url + "/feedback/create", data);
-  }
+    addNewFeedback(data) {
+        console.log("oui");
+        return axios.post(this.url + "/feedback/create", data);
+    }
 
-  deleteFeedback(id) {
-    return axios.delete(this.url + `/feedback/delete/${id}`);
-  }
+    deleteFeedback(id) {
+        return axios.delete(this.url + `/feedback/delete/${id}`);
+    }
 }
 
 export default new FeedbackService();
